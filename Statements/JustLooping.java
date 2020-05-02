91791427f53ba2792da69c0d7d0df06b9b273428
Chidozie Nnachor <odogwu.naija@gmail.com>
2020-05-02 14:53:03 +0200

    01-02-2020

--- Statements/JustLooping.java
+++ Statements/JustLooping.java
@@ -14,5 +14,12 @@ public class JustLooping {
 
   public static void main(String[] args) {
     new JustLooping().showJ();
+
+    byte starting = 3;
+    short firstValue = 5;
+    int secondValue = 7;
+    int functionValue = (int) (starting / 2 + firstValue / 2 + (int) firstValue / 3) + secondValue / 2;
+    int comp = (3 / 2 + 5 / 2 + 5 / 3) + 7 / 2;
+    System.out.println(comp);
   }
 }
