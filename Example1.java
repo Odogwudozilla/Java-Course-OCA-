2ebdc205bd544d188b19e3257ab9c95cda9ec599
Chidozie Nnachor <odogwu.naija@gmail.com>
2020-05-28 23:51:52 +0200

    static variable

--- Example1.java
+++ Example1.java
@@ -1,5 +1,11 @@
 public class Example1 {
 
+  static int count;
+
+  public Example1() {
+    count++;
+  }
+
   public static void main(String[] args) {
     int number = 1;
 
@@ -13,11 +19,19 @@ public class Example1 {
 
     System.out.println(number(number) + " " + letters);
 
-    String string = "odogwudozilla";
-    System.out.println(string.indexOf('o')); // 0
-    System.out.println(string.indexOf("zi")); // 4
-    System.out.println(string.indexOf('o', 5)); // 4
-    System.out.println(string.indexOf("gw", 9)); // -1
+    String one = new String();
+    StringBuilder two = new StringBuilder();
+    String three = one + "a";
+    // System.out.println(one == two); // false
+    System.out.println(one == three); // true
+
+    Example1 ex = new Example1();
+    Example1 ex1 = new Example1();
+    Example1 ex2 = new Example1();
+    Example1 ex3 = new Example1();
+    Example1 ex4 = new Example1();
+
+    System.out.println(count);
   }
 
   public static int number(int number) {
