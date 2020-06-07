1e86b56a3b7bc607298a6fefbf6439f04cb413ff
Chidozie Nnachor <odogwu.naija@gmail.com>
2020-06-07 18:46:12 +0200

    dumpss

--- dumps/VarPrimitive.java
+++ dumps/VarPrimitive.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class VarPrimitive {
   public static void main(String[] args) {
@@ -16,5 +18,12 @@ public class VarPrimitive {
     System.out.println(iVar);
     System.out.println(fVar);
     System.out.println(dVar);
+
+    LocalDate date1 = LocalDate.now();
+    LocalDate date2 = LocalDate.of(2014, 6, 20);
+    LocalDate date3 = LocalDate.parse("20140620", DateTimeFormatter.BASIC_ISO_DATE);
+    System.out.println("date1 = " + date1);
+    System.out.println("date2 = " + date2);
+    System.out.println("date3 = " + date3);
   }
 }
